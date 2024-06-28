35a8c1401b9bbffc981716b5a5b312dc1485a6db
asdewq22716 <annoplessan@gmail.com>
2024-06-28 10:10:10 +0700

    version 1.0.1

---
+++ inite.sql
@@ -0,0 +1,19 @@
+GRANT ALL PRIVILEGES ON *.* TO 'Annop'@'%';
+CREATE TABLE user_patiend (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    idcard VARCHAR(13),
+    firstname VARCHAR(50),
+    lastname VARCHAR(50),
+    age INT
+);
+-- แทรกข้อมูลคนที่ 1
+INSERT INTO user_patiend (idcard, firstname, lastname, age)
+VALUES ('1234567890123', 'John', 'Doe', 30);
+
+-- แทรกข้อมูลคนที่ 2
+INSERT INTO user_patiend (idcard, firstname, lastname, age)
+VALUES ('9876543210987', 'Jane', 'Smith', 25);
+
+-- แทรกข้อมูลคนที่ 3
+INSERT INTO user_patiend (idcard, firstname, lastname, age)
+VALUES ('5678901234567', 'Michael', 'Johnson', 40);
